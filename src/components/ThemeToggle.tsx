6dcfb8d1e8012fa72e8d776f7f19ef92f05125bf
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-07 15:42:45 +0330

    refactore: theme toggle btn

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SunIcon from "./icons/SunIcon";
 import MoonIcon from "./icons/MoonIcon";
 
 const ThemeToggle = () => {
-  const [theme, setTheme] = useState<string>("light");
+  const [theme, setTheme] = useState<string>(() => {
+    const saved = localStorage.getItem("theme");
+
+    if (!saved) {
+      localStorage.setItem("theme", "light");
+      return "light";
+    }
+
+    return JSON.parse(localStorage.getItem("theme")!);
+  });
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
   return (
     <>
       <button
