b3ca8691a7653b71e5534202aabe317c3e2a094d
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 14:02:22 +0330

    feat: add get notes api request using react useEffect and axios

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,35 @@ import Loading from "./components/ui/Loading";
 import { useEffect, useState } from "react";
 import { axiosInstance } from "./lib/axios";
 import type { Note } from "./types/note.type";
+import type { Response } from "./types/response.type";
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
   const [isFetchingNotes, setIsFetchingNotes] = useState<boolean>(false);
+  useEffect(() => {
+    const fetchNotes = async () => {
+      setIsFetchingNotes(true);
+
+      try {
+        const res = await axiosInstance.get<Response<Note[]>>("/api/notes");
+        setNotes(res.data.response ?? []);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsFetchingNotes(false);
+      }
+    };
+    fetchNotes();
+  }, []);
+
+  if (isFetchingNotes) return <Loading />;
+
   return (
     <>
       <Header />
       <main>
         <Container>
           <NoteList notes={notes} />
-          <Loading />
         </Container>
       </main>
     </>
