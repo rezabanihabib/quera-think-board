df0f3318e7565937f47d0679a36f0987e4dde1e9
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 13:07:35 +0330

    feat: add notes

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -15,6 +15,48 @@ const notes: Note[] = [
     createdAt: "2026-08-08T18:07:23.269Z",
     updatedAt: "2026-08-08T18:07:23.269Z",
   },
+  {
+    id: "cmskr5z0e110004jsx229at01",
+    title: "test",
+    content: "test1dfghghjj",
+    createdAt: "2026-08-08T19:14:27.326Z",
+    updatedAt: "2026-08-08T19:14:27.326Z",
+  },
+  {
+    id: "cmskorq11220904k1y4gq8rsm",
+    title: "test",
+    content: "test content",
+    createdAt: "2026-08-08T18:07:23.269Z",
+    updatedAt: "2026-08-08T18:07:23.269Z",
+  },
+  {
+    id: "cmskr5z0e330004jsx229at01",
+    title: "test",
+    content: "test1dfghghjj",
+    createdAt: "2026-08-08T19:14:27.326Z",
+    updatedAt: "2026-08-08T19:14:27.326Z",
+  },
+  {
+    id: "cmskorq11440904k1y4gq8rsm",
+    title: "test",
+    content: "test content",
+    createdAt: "2026-08-08T18:07:23.269Z",
+    updatedAt: "2026-08-08T18:07:23.269Z",
+  },
+  {
+    id: "cmskr5z0e555004jsx229at01",
+    title: "test",
+    content: "test1dfghghjj",
+    createdAt: "2026-08-08T19:14:27.326Z",
+    updatedAt: "2026-08-08T19:14:27.326Z",
+  },
+  {
+    id: "cmskorq11666904k1y4gq8rsm",
+    title: "test",
+    content: "test content",
+    createdAt: "2026-08-08T18:07:23.269Z",
+    updatedAt: "2026-08-08T18:07:23.269Z",
+  },
 ];
 
 export default notes;
