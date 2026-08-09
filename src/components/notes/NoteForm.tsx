c784eca19d82c2704bd8544da086693ece3082e1
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 14:05:04 +0330

    fix: add container to NoteForm

--- src/components/notes/NoteForm.tsx
+++ src/components/notes/NoteForm.tsx
@@ -1,29 +1,34 @@
 import { useState } from "react";
+import Container from "../layouts/Container";
 
 const NoteForm = () => {
   const [formValues, setFormValues] = useState({ title: "", content: "" });
   return (
-    <form className="my-5 space-y-3">
-      <input
-        type="text"
-        placeholder="enter your title here"
-        className="input w-full"
-        value={formValues.title}
-        onChange={(e) =>
-          setFormValues({ ...formValues, title: e.target.value })
-        }
-      />
+    <Container>
+      <form className="my-10 space-y-3">
+        <input
+          type="text"
+          placeholder="enter your title here"
+          className="input w-full"
+          value={formValues.title}
+          onChange={(e) =>
+            setFormValues({ ...formValues, title: e.target.value })
+          }
+        />
 
-      <textarea
-        placeholder="enter your note content here"
-        className="input w-full h-20 resize-none"
-        value={formValues.content}
-        onChange={(e) =>
-          setFormValues({ ...formValues, content: e.target.value })
-        }
-      ></textarea>
-      <button type="submit" className="btn btn-primary">create new note</button>
-    </form>
+        <textarea
+          placeholder="enter your note content here"
+          className="input w-full h-20 resize-none"
+          value={formValues.content}
+          onChange={(e) =>
+            setFormValues({ ...formValues, content: e.target.value })
+          }
+        ></textarea>
+        <button type="submit" className="btn btn-primary">
+          create new note
+        </button>
+      </form>
+    </Container>
   );
 };
 
