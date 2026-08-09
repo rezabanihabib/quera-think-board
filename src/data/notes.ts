e65d53c94c69be22fdc4e2efcf9cd82eb86040ec
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 12:56:14 +0330

    feat: add notes data

---
+++ src/data/notes.ts
@@ -0,0 +1,20 @@
+import type { Note } from "../types/note.type";
+
+const notes: Note[] = [
+  {
+    id: "cmskr5z0e000004jsx229at01",
+    title: "test",
+    content: "test1dfghghjj",
+    createdAt: "2026-08-08T19:14:27.326Z",
+    updatedAt: "2026-08-08T19:14:27.326Z",
+  },
+  {
+    id: "cmskorq11000904k1y4gq8rsm",
+    title: "test",
+    content: "test content",
+    createdAt: "2026-08-08T18:07:23.269Z",
+    updatedAt: "2026-08-08T18:07:23.269Z",
+  },
+];
+
+export default notes;
