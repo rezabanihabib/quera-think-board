854b618876348ebf0529ac70bec37c46659e9633
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 13:23:39 +0330

    feat: add NoteForm component

---
+++ src/components/notes/NoteForm.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const NoteForm = () => {
+  const [formValues, setFormValues] = useState({ title: "", content: "" });
+  return <div>NoteForm</div>;
+};
+
+export default NoteForm;
