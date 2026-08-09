ffc27923d05d11bfd270d0e094072673aee4160b
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 13:30:04 +0330

    feat: add NoteForm ui

--- src/components/notes/NoteForm.tsx
+++ src/components/notes/NoteForm.tsx
@@ -2,7 +2,29 @@ import { useState } from "react";
 
 const NoteForm = () => {
   const [formValues, setFormValues] = useState({ title: "", content: "" });
-  return <div>NoteForm</div>;
+  return (
+    <form className="my-5 space-y-3">
+      <input
+        type="text"
+        placeholder="enter your title here"
+        className="input w-full"
+        value={formValues.title}
+        onChange={(e) =>
+          setFormValues({ ...formValues, title: e.target.value })
+        }
+      />
+
+      <textarea
+        placeholder="enter your note content here"
+        className="input w-full h-20 resize-none"
+        value={formValues.content}
+        onChange={(e) =>
+          setFormValues({ ...formValues, content: e.target.value })
+        }
+      ></textarea>
+      <button type="submit" className="btn btn-primary">create new note</button>
+    </form>
+  );
 };
 
 export default NoteForm;
