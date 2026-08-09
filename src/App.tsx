fd8ab1ca7d478e9ee7220cff94cdff8b67b964ec
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 13:51:00 +0330

    feat: declaring states for getting notes from api

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import Container from "./components/layouts/Container";
 import NoteList from "./components/notes/NoteList";
 import Header from "./components/ui/Header";
 import Loading from "./components/ui/Loading";
-import notes from "./data/notes";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "./lib/axios";
+import type { Note } from "./types/note.type";
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [isFetchingNotes, setIsFetchingNotes] = useState<boolean>(false);
   return (
     <>
       <Header />
