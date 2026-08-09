d3e2121539e78226d36f03ae08588a433c6419d4
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 12:53:34 +0330

    feat: make NoteList dynamic

--- src/components/notes/NoteList.tsx
+++ src/components/notes/NoteList.tsx
@@ -1,12 +1,22 @@
 import type { FC } from "react";
 import type { Note } from "../../types/note.type";
+import { Container } from "lucide-react";
+import NoteCard from "./NoteCard";
 
 interface NoteListProps {
   notes: Note[];
 }
 
 const NoteList: FC<NoteListProps> = ({ notes }) => {
-  return <div>NoteList</div>;
+  return (
+    <section>
+      <Container className="my-5">
+        {notes.map((note) => (
+          <NoteCard note={note} />
+        ))}
+      </Container>
+    </section>
+  );
 };
 
 export default NoteList;
