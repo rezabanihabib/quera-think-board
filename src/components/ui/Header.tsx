543ffec738ca271bd1dd5d82326d9ed6349ce120
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 12:00:57 +0330

    refactore: add container to header component

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,11 +1,13 @@
+import Container from "../layouts/Container";
 import ThemeToggle from "./ThemeToggle";
-
 
 const Header = () => {
   return (
-    <header className="bg-base-300 py-4 px-5 flex items-center justify-between">
-      <h1 className="font-black">Think Board</h1>
-      <ThemeToggle />
+    <header className="bg-base-300 py-4">
+      <Container className="flex items-center justify-between">
+        <h1 className="font-black">Think Board</h1>
+        <ThemeToggle />
+      </Container>
     </header>
   );
 };
