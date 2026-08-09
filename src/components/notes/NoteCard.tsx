854de30e819b7899362e20cf21444d57b4b91833
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 12:40:23 +0330

    feat: make title and content dynamic

--- src/components/notes/NoteCard.tsx
+++ src/components/notes/NoteCard.tsx
@@ -9,11 +9,8 @@ const NoteCard: FC<NoteCardProps> = ({ note }) => {
   return (
     <div className="card card-border bg-base-100 w-96">
       <div className="card-body">
-        <h2 className="card-title">Card Title</h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
+        <h2 className="card-title">{note.title}</h2>
+        <p>{note.content}</p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Buy Now</button>
         </div>
