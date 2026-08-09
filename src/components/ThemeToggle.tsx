9c20af5deee8d915457f39e50a642b0a53bc1575
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-09 11:49:23 +0330

    refactore: move icon folder to layout directory

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import SunIcon from "./icons/SunIcon";
-import MoonIcon from "./icons/MoonIcon";
+import SunIcon from "./layouts/icons/SunIcon";
+import MoonIcon from "./layouts/icons/MoonIcon";
 
 const ThemeToggle = () => {
   const [theme, setTheme] = useState<string>(() => {
